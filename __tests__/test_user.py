e138e7764ebf9f52fe4b195ae95a22ad1d785b1d
arthurguedes <arthur.guedes@hotmail.com>
2024-06-22 02:04:21 -0300

    alterações feitas

--- __tests__/test_user.py
+++ __tests__/test_user.py
@@ -13,7 +13,7 @@ user_phone = "11952013248"
 user_status = 1
 
 url= 'https://petstore.swagger.io/v2/user'                 
-headers= {'Content-type':"application/json"}  
+headers= {'Content-type':'application/json'}  
 
 def test_post_user():
     user = open('./fixtures/json/user1.json')         # abertura de arquivo json 
@@ -32,23 +32,23 @@ def test_post_user():
     
 
     assert response.status_code == 200 
-    #assert response_body['username'] == user_username
-    #assert response_body['firstName'] == user_firstname
-    #assert response_body['lastName'] == user_lastname
+    assert response_body['code'] == 200
+    assert response_body['type'] == "unknown"
+    assert response_body['message'] == str(user_id)
 
 def test_get_user():
 
     response = requests.get(
         url=f'{url}/{user_username}',
-        headers=headers,
+        headers=headers
     )
 
     response_body = response.json()
 
     assert response.status_code == 200
-    #assert response_body['username'] == user_username
-    #assert response_body['firstName'] == user_firstname
-    #assert response_body['lastName'] == user_lastname
+    assert response_body['username'] == user_username
+    assert response_body['firstName'] == user_firstname
+    assert response_body['lastName'] == user_lastname
 
 
 def test_put_user():
@@ -66,9 +66,9 @@ def test_put_user():
     response_body = response.json()
 
     assert response.status_code == 200 
-    #assert response_body['username'] == user_username
-    #assert response_body['firstName'] == user_firstname
-    #assert response_body['lastName'] == user_lastname
+    assert response_body['code'] == 200
+    assert response_body['type'] == "unknown"
+    assert response_body['message'] == str(user_id)
 
 def test_delete_user():
 
@@ -94,7 +94,7 @@ def test_delete_user():
 
 def test_post_user_dinamico(user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status):
     user = {}     # lista vazia pet 
-    user['id'] = int(user_id)
+    user['id'] = user_id
     user['username'] = user_username
     user['firstName'] = user_firstname
     user['lastName'] = user_lastname
@@ -120,6 +120,64 @@ def test_post_user_dinamico(user_id,user_username,user_firstname,user_lastname,u
     response_body = response.json()
 
     assert response.status_code == 200
+    assert response_body['code'] == 200
+    assert response_body['type'] == "unknown"
+    assert response_body['message'] == str(user_id)
+
+    @pytest.mark.parametrize('user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status',
+                         ler_csv('./fixtures/csv/user.csv')
+    )
+    def test_get_user_dinamico(user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status):
+        
+        response = requests.get(
+            url=f'{url}/{user_username}',
+            headers=headers
+        )
+
+        response_body = response.json()
+
+        assert response.status_code == 200
+        assert response_body['id'] == int(user_id)
+        assert response_body['username'] == user_username
+        assert response_body['firstName'] == user_firstname
+
+
+@pytest.mark.parametrize('user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status',
+                         ler_csv('./fixtures/csv/user.csv')
+    )
+def test_put_user_dinamico(user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status):
+        
+        user = {}     # lista vazia pet 
+        user['id'] = user_id
+        user['username'] = user_username
+        user['firstName'] = user_firstname
+        user['lastName'] = user_lastname
+        user['email'] = user_email
+        user['password'] = user_password
+        user['phone'] = user_phone
+        user['user_Status'] = '0'
+
+        user = json.dumps(obj=user, indent=4)
+        print('\n' + user)
+
+        response = requests.put(
+            url=f'{url}/{user_username}',
+            headers=headers,
+            data=user,
+            timeout=5
+        )
+
+        response_body = response.json()
+
+        assert response.status_code == 200
+        assert response_body['code'] == 200
+        assert response_body['type'] == "unknown"
+        assert response_body['message'] == str(user_id)
+        
+
+
+
+
 
 
 
@@ -129,10 +187,10 @@ def test_post_user_dinamico(user_id,user_username,user_firstname,user_lastname,u
 
 
 
-def test_post_user_dinamico(user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status):
-    
+def test_delete_user_dinamico(user_id,user_username,user_firstname,user_lastname,user_email,user_password,user_phone,user_status):
+    '''
     user = {}     # lista vazia pet 
-    user['id'] = int(user_id)
+    user['id'] = user_id
     user['username'] = user_username
     user['firstName'] = user_firstname
     user['lastName'] = user_lastname
@@ -140,19 +198,17 @@ def test_post_user_dinamico(user_id,user_username,user_firstname,user_lastname,u
     user['password'] = user_password
     user['phone'] = user_phone
     user['user_Status'] = user_status
-    
+    '''
 
-
-    #user = json.dumps(obj=user, indent=4)
-    #print('\n' + user)                     # visualiza o json criado dinamicamente 
-
+    '''
+    user = json.dumps(obj=user, indent=4)
+    print('\n' + user)                     # visualiza o json criado dinamicamente 
+    '''
     #executa 
 
     response = requests.delete(
         url=f'{url}/{user_username}',
-        headers=headers,
-        data=user,
-        timeout=5
+        headers=headers
     )
     #compara
     response_body = response.json()
